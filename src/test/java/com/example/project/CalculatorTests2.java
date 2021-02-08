bdcc87e437d539adb17cbd33ab7db239ab93d1ee
Maximilian Vino <max.vinsum@gmail.com>
2021-02-08 15:21:42 -0400

    adad

--- src/test/java/com/example/project/CalculatorTests2.java
+++ src/test/java/com/example/project/CalculatorTests2.java
@@ -17,7 +17,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-class CalculatorTests {
+class CalculatorTests2 {
 
 	@Test
 	@DisplayName("2 + 2 = 4")
